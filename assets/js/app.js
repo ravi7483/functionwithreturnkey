5432bf0b6bcc02eb18495ee2bf331f4af6c5683d
ravi7483 <somwadvaibhav@gmail.com>
2022-04-07 19:13:53 +0530

    functiowithreturnkey

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+function Add2Num(x,y){
+	result = x + y;
+	console.log(result);
+	// console.log(result);
+	return result;
+	// console.log(result);
+	
+	
+}
+var result= Add2Num(10,10)
+console.log(result);
+document.write(result);
+// alert(result);
+
+// return stiops the exucytion of the function
+
+function getValueOfPI(){
+	return  Math.PI;
+}
+var p2 = getValueOfPI();
+console.log(p2);
+
+function getValueofPI(){
+	var result = Math.PI * 2; 
+	return Math.PI;
+}
+var p1 = getValueofPI();
+console.log(p1);
